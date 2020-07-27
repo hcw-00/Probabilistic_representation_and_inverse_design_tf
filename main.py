fcc6b72b7babbf770ca9d9cfe0c43f181124db8d
unknown <21908600@ncbnet.co.kr>
2020-07-27 14:05:37 +0900

    -

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def get_params():
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('--dataset_dir', dest='dataset_dir', default='train_100', help='path of the dataset') # 200411_HKC_3rd
     parser.add_argument('--epoch', dest='epoch', type=int, default=1000, help='# of epoch')
-    parser.add_argument('--epoch_step', dest='epoch_step', type=int, default=1000, help='# of epoch to decay lr')
+    parser.add_argument('--epoch_step', dest='epoch_step', type=int, default=500, help='# of epoch to decay lr')
     parser.add_argument('--batch_size', dest='batch_size', type=int, default=128, help='# images in batch')
     parser.add_argument('--train_size', dest='train_size', type=int, default=1e8, help='# images used to train')
     parser.add_argument('--lr', dest='lr', type=float, default=0.00001, help='initial learning rate for adam') # default=0.0002
@@ -20,7 +20,7 @@ def get_params():
     parser.add_argument('--continue_train', dest='continue_train', type=bool, default=False, help='if continue training, load the latest model: 1: true, 0: false')
     parser.add_argument('--pj_dir', dest='pj_dir', default='./', help='sample, logs, models are saved here')
     parser.add_argument('--L1_lambda', dest='L1_lambda', type=float, default=10.0, help='weight on L1 term in objective')
-    parser.add_argument('--alpha', dest='alpha', type=float, default=10000.0, help='weight on regression loss')
+    parser.add_argument('--alpha', dest='alpha', type=float, default=5000.0, help='weight on regression loss')
     parser.add_argument('--beta', dest='beta', type=int, default=10000, help='weight on kl div loss')
     parser.add_argument('--latent_dims', dest='latent_dims', type=int, default=5, help='latent space dimension')
 
